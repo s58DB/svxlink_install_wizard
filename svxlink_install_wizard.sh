fa6c55a4dc155066667e140db7e6fa2882705ca1
Danilo Bozic <s58db.danilo@gmail.com>
2022-09-17 21:06:48 +0200

    Update svxlink_install_wizard.sh

--- svxlink_install_wizard.sh
+++ svxlink_install_wizard.sh
@@ -8,11 +8,11 @@ function displayMessage() {
 
 function main() {
 while true; do
-        menusvxlink=$(whiptail --title "Pomocnik za namesteitev SVXlink-a" --ok-button "Select" --cancel-button "Quit" --menu "Izberi namestitev" 20 78 10 \
-		"1" "Namestitev SVXLink za RPi" \
-		"2" "Namestitev SVXLink za PC" \
-		"3" "Odstrani SVXLink za RPi in PC" \
-		"4" "Kopija SVXLinka" \
+        menusvxlink=$(whiptail --title "...::::: SVXlink Installing Wizard :::::..." --ok-button "Select" --cancel-button "Quit" --menu "Izberi namestitev" 20 78 10 \
+		"1" "..:: Install SVXLink on RPi ::.." \
+		"2" "..:: Install SVXLink on PC ::.." \
+		"3" "..:: Copy importatnt folders SVXLink ::.." \
+		"4" "..:: Remove SVXLink folder from system ::.." \
             3>&1 1>&2 2>&3)
 
         exitstatus=$?
@@ -23,10 +23,10 @@ while true; do
 			svxlinkInstallRpi
                 ;;
 		2)
-			svxlinkInstallPC_test
+			svxlinkInstallPC
 		;;
 		3)
-			svxlinkUninstallRpi
+			svxlinkUninstall
 		;;
 		4)
 			svxlinkcopyall
@@ -84,30 +84,7 @@ function svxlinkInstallPC() {
 	 whiptail --msgbox "Namestitev je koncana..." 10 50
 }
 
-function svxlinkInstallPC_test() {
-
-                cd ~
-                apt-get update && sudo upgrade -y
-                apt-get install build-essential wget g++ cmake make libsigc++-2.0-dev libgsm1-dev libpopt-dev tcl-dev libgcrypt20-dev libspeex-dev libasound2-dev libopus-dev librtlsdr-dev doxygen groff alsa-utils vorbis-tools curl libcurl4-openssl-dev git rtl-sdr libjsoncpp-dev speedtest-cli mutt mc alsa-utils -y
-                useradd -U -r -G audio,plugdev,daemon svxlink
-                cd ~
-                git clone https://github.com/sm0svx/svxlink.git
-                mkdir ~/svxlink/src/build
-                cd ~/svxlink/src/build
-                cmake -DUSE_QT=OFF -DCMAKE_INSTALL_PREFIX=/usr -DSYSCONF_INSTALL_DIR=/etc -DLOCAL_STATE_DIR=/var -DWITH_SYSTEMD=ON ..
-                make
-                make doc
-                make install
-                ldconfig
-                cd /usr/share/svxlink/sounds/
-                wget https://github.com/sm0svx/svxlink-sounds-en_US-heather/releases/download/19.09.99.2/svxlink-sounds-en_US-heather-16k-19.09.99.2.tar.bz2
-                tar xvjf svxlink-sounds-en_US-heather-16k-19.09.99.2.tar.bz2
-                mv en_US-heather-16k en_US
-	whiptail --msgbox "Namestitev je koncana..." 10 50
-}
-
-
-function svxlinkUninstallRpi() {
+function svxlinkUninstall() {
 
                 rm -rf ~/svxlink/
                 sleep 1
@@ -124,38 +101,20 @@ function svxlinkcopyall() {
 
                 cd ~
 		mkdir "Kopija_SVXLink_$(date +"%d_%m_%Y")"
-		echo OK
-		sleep 5
                 cd Kopija_SVXLink_$(date +"%d_%m_%Y")
 		mkdir etc
 		mkdir etc/svxlink
-		echo OK
-		 sleep 5
 		mkdir usr
 		mkdir usr/share
 		mkdir usr/share/svxlink
-		echo OK
-		 sleep 5
 		mkdir var
 		mkdir var/spool
 		mkdir var/spool/svxlink
-		echo OK
-		 sleep 5 
 		cp -f -r /etc/svxlink/* etc/svxlink/ 
-		echo OK
-		 sleep 5
 		cp -f -r /usr/share/svxlink/* usr/share/svxlink/
-		echo OK
-		 sleep 5
 		cp -f -r /var/spool/svxlink/* var/spool/svxlink/
-		echo OK
-		 sleep 5
 		cd ~
-		echo OK
-		 sleep 5
 		chown -hR pi:pi Kopija_SVXLink_$(date +"%d_%m_%Y")
-		echo ok
-		 sleep 5
         whiptail --msgbox "Odstranitev je koncana..." 10 50
 }
 
