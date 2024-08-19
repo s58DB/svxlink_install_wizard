e6ed0bc03557ca57ae2ac5ecdec4e0fbd69c00b7
Danilo Bozic <s58db.danilo@gmail.com>
2024-08-19 23:01:57 +0200

    Update svxlink_install_wizard.sh

--- svxlink_install_wizard.sh
+++ svxlink_install_wizard.sh
@@ -45,7 +45,7 @@ while true; do
 function svxlinkInstallRpi() {
 		cd ~
                 sudo apt update && sudo apt full-upgrade -y
-                sudo apt-get install build-essential wget g++ cmake make libsigc++-2.0-dev libgsm1-dev libpopt-dev tcl-dev libgcrypt20-dev libspeex-dev libasound2-dev libopus-dev librtlsdr-dev doxygen groff alsa-utils vorbis-tools curl libcurl4-openssl-dev git rtl-sdr libjsoncpp-dev speedtest-cli mutt mc libgpiod-dev gpiod libssl-dev -y
+                sudo apt-get install build-essential wget g++ cmake make libsigc++-2.0-dev libgsm1-dev libpopt-dev tcl-dev libgcrypt20-dev libspeex-dev libasound2-dev libopus-dev librtlsdr-dev doxygen groff alsa-utils vorbis-tools curl libcurl4-openssl-dev git rtl-sdr libjsoncpp-dev speedtest-cli mutt mc libgpiod-dev gpiod libssl-dev libgpiod-dev gpiod -y
                 sudo useradd -U -r -G audio,plugdev,daemon,gpio svxlink
                 cd ~
 		git clone https://github.com/sm0svx/svxlink.git
