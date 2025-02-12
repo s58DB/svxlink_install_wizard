5874f474162e7e91e1e3dfe7196726cbf80b8dbb
Danilo Bozic <s58db.danilo@gmail.com>
2025-02-12 21:01:51 +0100

    Update svxlink_install_wizard_beta.sh
    with Whiptail

--- svxlink_install_wizard_beta.sh
+++ svxlink_install_wizard_beta.sh
@@ -1,125 +1,132 @@
 #!/usr/bin/env bash
-set -e  # Ustavi skripto, če pride do napake
 
-## Prikaz sporočila v terminalu
-function display_message() {
-    echo "[INFO] $1: $2"
+## Author: Danilo - S58DB
+## E-mail: s58db.danilo@gmail.com
+
+## Preveri, ali je whiptail nameščen
+if ! command -v whiptail &> /dev/null; then
+    echo "Whiptail ni nameščen. Namesti ga s: sudo apt install whiptail"
+    exit 1
+fi
+
+## Funkcija za prikaz sporočila v Whiptail
+function displayMessage() {
+    whiptail --title "$1" --msgbox "$2" 8 78
 }
 
-## Glavni meni
-function main() {
-    while true; do
-        echo "[INFO] Prikaz glavnega menija..."
-        echo "1) Install SVXLink on RPi"
-        echo "2) Install SVXLink on PC"
-        echo "3) Copy important SVXLink folders"
-        echo "4) Remove SVXLink folder from system"
-        echo "5) Exit"
-
-        read -p "Izberi možnost: " menusvxlink
-        case ${menusvxlink} in
-            1) install_svxlink_rpi ;;
-            2) install_svxlink_pc ;;
-            3) copy_svxlink_files ;;
-            4) uninstall_svxlink ;;
-            5) exit ;;
-            *) echo "[NAPAKA] Napačna izbira!";;
-        esac
-    done
-}
-
-## Ustvari ali posodobi uporabnika 'svxlink' z ustreznimi skupinami
-function create_svxlink_user() {
-    local user_groups="$1"  # Sprejme seznam skupin kot argument (različne za PC/RPi)
-
-    if id "svxlink" &>/dev/null; then
-        echo "[INFO] User 'svxlink' already exists, updating groups..."
-        sudo usermod -aG $user_groups svxlink
-    else
-        echo "[INFO] Creating user 'svxlink'..."
-        sudo useradd -U -r -G $user_groups svxlink
-    fi
-}
-
-## Namestitev SVXLink na Raspberry Pi
+## Namestitev SVXLink na RPi
 function install_svxlink_rpi() {
-    echo "[INFO] Začenjam namestitev SVXLink na RPi..."
-
     cd ~
     sudo apt update && sudo apt full-upgrade -y
-    sudo apt-get install -y build-essential wget g++ cmake make libsigc++-2.0-dev libgsm1-dev \
-        libpopt-dev tcl-dev libgcrypt20-dev libspeex-dev libasound2-dev libopus-dev librtlsdr-dev \
-        doxygen groff alsa-utils vorbis-tools curl libcurl4-openssl-dev git rtl-sdr libjsoncpp-dev \
-        speedtest-cli mutt mc libssl-dev libgpiod-dev gpiod
+    sudo apt-get install -y build-essential wget g++ cmake make libsigc++-2.0-dev \
+        libgsm1-dev libpopt-dev tcl-dev libgcrypt20-dev libspeex-dev libasound2-dev \
+        libopus-dev librtlsdr-dev doxygen groff alsa-utils vorbis-tools curl \
+        libcurl4-openssl-dev git rtl-sdr libjsoncpp-dev speedtest-cli mutt mc \
+        libgpiod-dev gpiod libssl-dev 
 
-    # Ustvari uporabnika (RPi verzija - VSE skupine, vključno z gpio)
-    create_svxlink_user "audio,plugdev,daemon,gpio"
+    sudo useradd -U -r -G audio,plugdev,daemon,gpio svxlink || echo "User already exists."
 
-    if [ ! -d "$HOME/svxlink" ]; then
-        git clone https://github.com/sm0svx/svxlink.git
-    else
-        echo "[INFO] SVXLink repozitorij že obstaja. Preskakujem kloniranje..."
-    fi
-
-    cd ~/svxlink
+    cd ~
+    git clone https://github.com/sm0svx/svxlink.git
+    cd svxlink
     git reset --hard 12c9d9b
-    mkdir -p ~/svxlink/src/build
-    cd ~/svxlink/src/build
-
+    mkdir ~/svxlink/src/build
+    cd ~/svxlink/src/build 
     cmake -DUSE_QT=OFF -DCMAKE_INSTALL_PREFIX=/usr -DSYSCONF_INSTALL_DIR=/etc \
-        -DLOCAL_STATE_DIR=/var -DWITH_SYSTEMD=ON ..
-    make || { echo "[NAPAKA] Make ni uspel! Izhod..."; exit 1; }
+          -DLOCAL_STATE_DIR=/var -DWITH_SYSTEMD=ON ..
+    make && make doc
     sudo make install
     sudo ldconfig
 
     cd /usr/share/svxlink/sounds/
-    sudo wget -q https://github.com/sm0svx/svxlink-sounds-en_US-heather/releases/download/24.02/svxlink-sounds-en_US-heather-16k-24.02.tar.bz2
+    sudo wget https://github.com/sm0svx/svxlink-sounds-en_US-heather/releases/download/24.02/svxlink-sounds-en_US-heather-16k-24.02.tar.bz2
     sudo tar xvjf svxlink-sounds-en_US-heather-16k-24.02.tar.bz2
     sudo mv en_US-heather-16k en_US
     sudo rm svxlink-sounds-en_US-heather-16k-24.02.tar.bz2
 
-    display_message "Installation Complete" "SVXLink has been successfully installed on Raspberry Pi."
+    displayMessage "Namestitev dokončana" "SVXLink je uspešno nameščen na RPi!"
 }
 
 ## Namestitev SVXLink na PC
 function install_svxlink_pc() {
-    echo "[INFO] Začenjam namestitev SVXLink na PC..."
+    cd ~
+    sudo apt-get update && sudo apt-get upgrade -y
+    sudo apt-get install -y build-essential wget g++ cmake make libsigc++-2.0-dev \
+        libgsm1-dev libpopt-dev tcl-dev libgcrypt20-dev libspeex-dev libasound2-dev \
+        libopus-dev librtlsdr-dev doxygen groff alsa-utils vorbis-tools curl \
+        libcurl4-openssl-dev git rtl-sdr libjsoncpp-dev speedtest-cli mutt mc alsa-utils 
+
+    sudo useradd -U -r -G audio,plugdev,daemon svxlink || echo "User already exists."
 
     cd ~
-    sudo apt update && sudo apt full-upgrade -y
-    sudo apt-get install -y build-essential wget g++ cmake make libsigc++-2.0-dev libgsm1-dev \
-        libpopt-dev tcl-dev libgcrypt20-dev libspeex-dev libasound2-dev libopus-dev librtlsdr-dev \
-        doxygen groff alsa-utils vorbis-tools curl libcurl4-openssl-dev git rtl-sdr libjsoncpp-dev \
-        speedtest-cli mutt mc libssl-dev
-
-    # Ustvari uporabnika (PC verzija - BREZ skupine gpio)
-    create_svxlink_user "audio,plugdev,daemon"
-
-    if [ ! -d "$HOME/svxlink" ]; then
-        git clone https://github.com/sm0svx/svxlink.git
-    else
-        echo "[INFO] SVXLink repozitorij že obstaja. Preskakujem kloniranje..."
-    fi
-
-    cd ~/svxlink
+    git clone https://github.com/sm0svx/svxlink.git
+    cd svxlink
     git reset --hard 12c9d9b
-    mkdir -p ~/svxlink/src/build
-    cd ~/svxlink/src/build
-
+    mkdir ~/svxlink/src/build
+    cd ~/svxlink/src/build 
     cmake -DUSE_QT=OFF -DCMAKE_INSTALL_PREFIX=/usr -DSYSCONF_INSTALL_DIR=/etc \
-        -DLOCAL_STATE_DIR=/var -DWITH_SYSTEMD=ON ..
-    make || { echo "[NAPAKA] Make ni uspel! Izhod..."; exit 1; }
+          -DLOCAL_STATE_DIR=/var -DWITH_SYSTEMD=ON ..
+    make && make doc
     sudo make install
     sudo ldconfig
 
     cd /usr/share/svxlink/sounds/
-    sudo wget -q https://github.com/sm0svx/svxlink-sounds-en_US-heather/releases/download/24.02/svxlink-sounds-en_US-heather-16k-24.02.tar.bz2
+    sudo wget https://github.com/sm0svx/svxlink-sounds-en_US-heather/releases/download/24.02/svxlink-sounds-en_US-heather-16k-24.02.tar.bz2
     sudo tar xvjf svxlink-sounds-en_US-heather-16k-24.02.tar.bz2
     sudo mv en_US-heather-16k en_US
     sudo rm svxlink-sounds-en_US-heather-16k-24.02.tar.bz2
 
-    display_message "Installation Complete" "SVXLink has been successfully installed on PC."
+    displayMessage "Namestitev dokončana" "SVXLink je uspešno nameščen na PC!"
 }
 
-## Zagon glavnega menija
+## Brisanje SVXLink iz sistema
+function uninstall_svxlink() {
+    sudo rm -rf ~/svxlink/ /etc/svxlink/ /var/spool/svxlink/ /usr/share/svxlink/
+    displayMessage "Brisanje dokončano" "SVXLink je bil odstranjen iz sistema!"
+}
+
+## Kopiranje pomembnih datotek SVXLink
+function copy_svxlink_files() {
+    cd ~
+    backup_dir="Kopija_SVXLink_$(date +"%d_%m_%Y")"
+    mkdir -p "$backup_dir/etc/svxlink" "$backup_dir/usr/share/svxlink" "$backup_dir/var/spool/svxlink"
+
+    cp -r /etc/svxlink/* "$backup_dir/etc/svxlink/"
+    cp -r /usr/share/svxlink/* "$backup_dir/usr/share/svxlink/"
+    cp -r /var/spool/svxlink/* "$backup_dir/var/spool/svxlink/"
+
+    chown -hR pi:pi "$backup_dir"
+
+    displayMessage "Varnostna kopija končana" "Varnostna kopija se nahaja v: $HOME/$backup_dir"
+}
+
+## Glavni meni z Whiptail
+function main() {
+    while true; do
+        menusvxlink=$(whiptail --title "...::::: SVXLink Namestitveni čarovnik :::::..." \
+            --ok-button "Izberi" --cancel-button "Izhod" \
+            --menu "Izberi namestitev" 20 78 10 \
+            "1" "..:: Namesti SVXLink na RPi ::.." \
+            "2" "..:: Namesti SVXLink na PC ::.." \
+            "3" "..:: Ustvari varnostno kopijo SVXLink ::.." \
+            "4" "..:: Odstrani SVXLink iz sistema ::.." \
+            "5" "..:: Izhod ::.." \
+            3>&1 1>&2 2>&3)
+
+        exitstatus=$?
+        if [ ${exitstatus} = 0 ]; then
+            case ${menusvxlink} in
+                1) install_svxlink_rpi ;;
+                2) install_svxlink_pc ;;
+                3) copy_svxlink_files ;;
+                4) uninstall_svxlink ;;
+                5) exit ;;
+            esac
+        else
+            exit
+        fi
+    done
+}
+
+## Zaženi glavni meni
 main
